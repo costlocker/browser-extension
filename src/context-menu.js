5a4dd4262b0f8178d04a3f81eb9a985ffeb8a139
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-07-18 09:07:25 +0200

    Revert "Spike context menu"
    This reverts commit 80ddb54e2c43fa79d1d3e2989b14d4bc2304c973.

--- src/context-menu.js
+++
@@ -1,9 +0,0 @@
-
-chrome.contextMenus.create({
-    "title": "Start tracking: '%s'",
-    "contexts": ["selection"],
-    "onclick": function (info) {
-        console.log(info.selectionText);
-        // cannot open popup: https://stackoverflow.com/a/33362655
-    }
-});
