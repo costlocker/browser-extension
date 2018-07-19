44942cbbf1e9baee3581346f7ac1589afaede3d5
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-07-19 13:30:20 +0200

    Design - searchable select, adapt css from app
    Cannot use 3.0.4
    https://github.com/jshjohnson/Choices/issues/377

--- src/popup.js
+++ src/popup.js
@@ -101,7 +101,7 @@ function showPopup () {
             reloadIcon();
             loadAssignments(data.Simple_Tracking_Assignments);
             if (isRunning()) {
-                document.getElementById('project-running').value = assignmentToString(runningEntry);
+                document.getElementById('project-running').innerHTML = renderAssignmentOption(runningEntry.names, 'class="choices__inner"');
                 document.getElementById('description-running').value = runningEntry.description;
                 showPage('page-tracking-stop');
             } else {
@@ -115,30 +115,85 @@ function showPopup () {
 function loadAssignments(availableAssignments) {
     const projectsSelect = document.getElementById('project-start');
     projectsSelect.innerHTML = '';
+
+    const options = [];
     availableAssignments.forEach((item, index) => {
-        const option = new Option(assignmentToString(item), index);
-        option.setAttribute('data-assignment', JSON.stringify(item.assignment));
-        projectsSelect.options[index] = option;
+        options.push({
+            value: JSON.stringify(item.assignment).replace(/"/g, "'"),
+            label: assignmentToString(item),
+            selected: index == 0,
+            customProperties: item.names,
+        })
     });
+
+    const choices = new Choices(projectsSelect, {
+        choices: options,
+        maxItemCount: 1,
+        searchResultLimit: 5,
+        position: 'bottom',
+        callbackOnCreateTemplates: function (template) {
+            var classNames = this.config.classNames;
+            const render = (buildContainer) => {
+                return (data) => {
+                    if (!data.customProperties.project_name) {
+                        return template(`
+                            <div ${buildContainer(data)}>
+                                <div class="timesheet-task-search__project">
+                                    [No project]
+                                </div>
+                                <div class="timesheet-task-search__activity-task">
+                                    Select a task...
+                                </div>
+                            </div>
+                        `);
+                    }
+                    return template(renderAssignmentOption(data.customProperties, buildContainer(data)));
+                  }
+            };
+            return {
+              item: render((data) => `class="${classNames.item} ${data.highlighted ? classNames.highlightedState : classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" ${data.active ? 'aria-selected="true"' : ''} ${data.disabled ? 'aria-disabled="true"' : ''}`),
+              choice: render((data) => `class="${classNames.item} ${classNames.itemChoice} ${data.disabled ? classNames.itemDisabled : classNames.itemSelectable}" data-select-text="${this.config.itemSelectText}" data-choice ${data.disabled ? 'data-choice-disabled aria-disabled="true"' : 'data-choice-selectable'} data-id="${data.id}" data-value="${data.value}" ${data.groupId > 0 ? 'role="treeitem"' : 'role="option"'}`),
+            };
+        },
+    });    
+    projectsSelect.addEventListener(
+        'hideDropdown',
+        function(event) {
+            document.body.parentElement.className = 'tracking-window'; // hack for popup height
+        },
+        false
+    );
+}
+
+function renderAssignmentOption(names, container) {
+    const task = names.task_name !== null ? ` - ${names.task_name}` : '';
+    return `
+        <div ${container}>
+            <div class="timesheet-task-search__project">
+                <span class="timesheet-task-search__client">${names.client_name}</span> ${names.project_name}
+            </div>
+            <div class="timesheet-task-search__activity-task">
+                ${names.activity_name} ${task}
+            </div>
+        </div>
+    `;
 }
 
 function assignmentToString(item) {
     if (!item.assignment.project_id) {
-        return '[No project]';
+        return '';
     }
     const task = item.assignment.task_id ? ` - ${item.names.task_name}` : '';
     return `${item.names.project_name} (${item.names.client_name}) - ${item.names.activity_name}${task}`;
 }
 
 function getSelectedAssignment() {
-    return JSON.parse(getSelectedOption().getAttribute('data-assignment'));
+    return JSON.parse(getSelectedOption().replace(/'/g, '"'));
 }
 
 function getSelectedOption() {
     const select = document.getElementById('project-start');
-    return select[select.selectedIndex || 0] || {
-        getAttribute: () => null
-    };
+    return select[select.selectedIndex || 0].value;
 }
 
 function loadDataFromCurrentPage() {
